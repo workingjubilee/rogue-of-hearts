a47aa168ca59dc588b5bcb200d2d4c1eb988f1eb
Jubilee Young <workingjubilee@gmail.com>
2020-03-03 20:12:24 -0800

    Porting sample code from RLTK tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,74 @@
-use bracket_lib::prelude::*;
-use bracket_lib::prelude::BTerm;
+use bracket_lib::prelude::{Console, GameState, BTerm, RGB};
+use specs::prelude::*;
+#[macro_use]
+extern crate specs_derive;
+mod components;
+pub use components::*;
+mod map;
+pub use map::*;
+mod player;
+use player::*;
+mod rect;
+pub use rect::Rect;
 
-struct State {}
-impl GameState for State {
-    fn tick(&mut self, ctx : &mut BTerm) {
-        ctx.cls();
-        ctx.print(1, 1, "Hello Bracket Lib!");
+
+
+pub struct State {
+    pub ecs: World
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        self.ecs.maintain();
     }
 }
 
-fn main() {
-    let context = BTerm::init_simple8x8(80, 50, "Hello Bracket Lib!", "resources");
-    let gs = State{ };
-    bracket_lib::prelude::main_loop(context, gs);
+impl GameState for State {
+    fn tick(&mut self, ctx : &mut BTerm) {
+        ctx.cls();
+
+        player_input(self, ctx);
+        self.run_systems();
+
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
+
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+    }
 }
 
+
+fn main() {
+    use bracket_lib::prelude::BTermBuilder;
+    let context = BTermBuilder::simple80x50()
+        .with_title("Roguelike Tutorial")
+        .build();
+    let mut gs = State {
+        ecs: World::new()
+    };
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+    gs.ecs.register::<Player>();
+
+    let (rooms, map) = new_map_rooms_and_corridors();
+    gs.ecs.insert(map);
+    let (player_x, player_y) = rooms[0].center();
+
+    gs.ecs
+        .create_entity()
+        .with(Position { x: player_x, y: player_y })
+        .with(Renderable {
+            glyph: bracket_lib::prelude::to_cp437('@'),
+            fg: RGB::named(bracket_lib::prelude::YELLOW),
+            bg: RGB::named(bracket_lib::prelude::BLACK),
+        })
+        .with(Player{})
+        .build();
+
+    bracket_lib::prelude::main_loop(context, gs);
+}
--- src/player.rs
+++ src/player.rs
@@ -1,2 +1,62 @@
+use bracket_lib::prelude::{VirtualKeyCode, BTerm};
+use specs::prelude::*;
+use super::{Position, Player, TileType, xy_idx, State};
+use std::cmp::{min, max};
 
-// Handle player and interface here?
+pub enum Direction {
+    N,
+    S,
+    W,
+    E,
+    NE,
+    NW,
+    SW,
+    SE,
+}
+
+
+pub fn try_move_player(dir: Direction, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+    let map = ecs.fetch::<Vec<TileType>>();
+    let (delta_x, delta_y) = match dir {
+        Direction::E => (1, 0),
+        Direction::S => (0, 1),
+        Direction::W => (-1, 0),
+        Direction::N => (0, -1),
+        Direction::SE => (1, 1),
+        Direction::SW => (-1, 1),
+        Direction::NE => (1, -1),
+        Direction::NW => (-1, -1),
+    };
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+        if map[destination_idx] != TileType::Wall {
+            pos.x = min(79 , max(0, pos.x + delta_x));
+            pos.y = min(49, max(0, pos.y + delta_y));
+        }
+    }
+}
+
+pub fn player_input(gs: &mut State, ctx: &mut BTerm) {
+    // Player movement
+    match ctx.key {
+        None => {} // Nothing happened
+        Some(key) => match key {
+            VirtualKeyCode::Left => try_move_player(Direction::W, &mut gs.ecs),
+            VirtualKeyCode::Numpad4 => try_move_player(Direction::W, &mut gs.ecs),
+            VirtualKeyCode::H => try_move_player(Direction::W, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(Direction::E, &mut gs.ecs),
+            VirtualKeyCode::Numpad6 => try_move_player(Direction::E, &mut gs.ecs),
+            VirtualKeyCode::L => try_move_player(Direction::E, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(Direction::N, &mut gs.ecs),
+            VirtualKeyCode::Numpad8 => try_move_player(Direction::N, &mut gs.ecs),
+            VirtualKeyCode::K => try_move_player(Direction::N, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(Direction::S, &mut gs.ecs),
+            VirtualKeyCode::Numpad2 => try_move_player(Direction::S, &mut gs.ecs),
+            VirtualKeyCode::J => try_move_player(Direction::S, &mut gs.ecs),
+            _ => {}
+        },
+    }
+}
